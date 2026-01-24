0282f0d1a2ffd34f1048e766f7352170f4d62bec
wiktor-t <wikcuse73@gmail.com>
2026-01-24 20:52:08 +0000

    Test local web server
    Testing if the ESP shows a web server GUI. After finding the ESP's IP address and entering it into a browser, the test web page loads successfully.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(
+    200,
+    "text/html",
+    "<p>ESP local web page test</p>"
+  );
+} 
 
 void setup() {
   WiFi.begin("wifi_name", "wifi_password");
   while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  server.on("/", handleRoot);
+  server.begin();
 }
 
-void loop() {}
+void loop() {
+  server.handleClient();
+}
+
+
